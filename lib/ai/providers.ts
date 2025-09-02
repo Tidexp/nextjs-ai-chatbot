e6ea80cd93d9ee568bf475af0f223ddb0533d9ec
NgayXuaEmNoi <duythaitran797@gmail.com>
2025-09-02 23:19:17 +0700

    Update providers.ts

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -1,26 +1,13 @@
 import { customProvider } from 'ai';
 import { groq } from '@ai-sdk/groq';
-import { isTestEnvironment } from '../constants';
-import {
-  artifactModel,
-  chatModel,
-  reasoningModel,
-  titleModel,
-} from './models.test';
 
-// myProvider config
-export const myProvider = isTestEnvironment
-  ? customProvider({
-      languageModels: {
-        'chat-model': chatModel,
-        'chat-model-reasoning': reasoningModel,
-        'title-model': titleModel,
-        'artifact-model': artifactModel,
-      },
-    })
-  : customProvider({
-      languageModels: {
-        'meta-llama/llama-guard-4-12b': groq('meta-llama/llama-guard-4-12b'),
-        'gemma2-9b-it': groq('gemma2-9b-it'),
-      },
-    });
+export const myProvider = customProvider({
+  languageModels: {
+    'meta-llama/llama-guard-4-12b': groq('meta-llama/llama-guard-4-12b', {
+      apiKey: process.env.XAI_API_KEY!,
+    }),
+    'gemma2-9b-it': groq('gemma2-9b-it', {
+      apiKey: process.env.XAI_API_KEY!,
+    }),
+  },
+});
