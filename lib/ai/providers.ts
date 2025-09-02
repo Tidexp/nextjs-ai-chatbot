8ed53be763fb818de5006b18c2b93ccc44fc88be
NgayXuaEmNoi <duythaitran797@gmail.com>
2025-09-02 21:08:34 +0700

    Update providers.ts

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -1,16 +1,12 @@
-import {
-  customProvider,
-  extractReasoningMiddleware,
-  wrapLanguageModel,
-} from 'ai';
-import { xai } from '@ai-sdk/xai';
+import { customProvider } from 'ai';
+import { openai } from '@ai-sdk/openai'; // dùng Groq API theo chuẩn OpenAI
+import { isTestEnvironment } from '../constants';
 import {
   artifactModel,
   chatModel,
   reasoningModel,
   titleModel,
 } from './models.test';
-import { isTestEnvironment } from '../constants';
 
 export const myProvider = isTestEnvironment
   ? customProvider({
@@ -23,15 +19,16 @@ export const myProvider = isTestEnvironment
     })
   : customProvider({
       languageModels: {
-        'chat-model': xai('grok-2-vision-1212'),
-        'chat-model-reasoning': wrapLanguageModel({
-          model: xai('grok-3-mini-beta'),
-          middleware: extractReasoningMiddleware({ tagName: 'think' }),
+        // match với chatModels id
+        'meta-llama/llama-guard-4-12b': openai.chat({
+          model: 'meta-llama/llama-guard-4-12b',
+          apiKey: process.env.XAI_API_KEY!,
+          baseURL: 'https://api.groq.com/openai/v1',
         }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),
-      },
-      imageModels: {
-        'small-model': xai.imageModel('grok-2-image'),
+        'gemma2-9b-it': openai.chat({
+          model: 'gemma2-9b-it',
+          apiKey: process.env.XAI_API_KEY!,
+          baseURL: 'https://api.groq.com/openai/v1',
+        }),
       },
     });
