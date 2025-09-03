e2337644720ce1654082703608d2f5586a9910a6
NgayXuaEmNoi <duythaitran797@gmail.com>
2025-09-03 21:25:53 +0700

    Update providers.ts

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -3,7 +3,7 @@ import { groq } from '@ai-sdk/groq';
 
 export const myProvider = customProvider({
   languageModels: {
-    'meta-llama/llama-guard-4-12b': groq('meta-llama/llama-guard-4-12b'),
+    'llama-guard-4-12b': groq('llama-guard-4-12b'),
     'gemma2-9b-it': groq('gemma2-9b-it'),
   },
 });
